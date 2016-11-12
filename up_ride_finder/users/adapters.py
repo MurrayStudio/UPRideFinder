e141a639e57c30cd159abec10b3d3f671029dc03
Ben Creighton <wackyapples@users.noreply.github.com>
2016-11-11 16:42:48 -0800

    Removed social log-in

--- up_ride_finder/users/adapters.py
+++ up_ride_finder/users/adapters.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
-from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+# from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 
 class AccountAdapter(DefaultAccountAdapter):
@@ -9,6 +9,6 @@ class AccountAdapter(DefaultAccountAdapter):
         return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
 
 
-class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def is_open_for_signup(self, request, sociallogin):
-        return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
+# class SocialAccountAdapter(DefaultSocialAccountAdapter):
+#     def is_open_for_signup(self, request, sociallogin):
+#         return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
