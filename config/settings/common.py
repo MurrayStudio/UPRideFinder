b6b2a84c00f50daa1f328b9caf92335616603cf2
Ben Creighton <bncreighton@gmail.com>
2017-02-15 19:19:52 -0800

    Changed cripsy template pack theme to bs3

--- config/settings/common.py
+++ config/settings/common.py
@@ -169,7 +169,7 @@ TEMPLATES = [
 ]
 
 # See: http://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 # STATIC FILE CONFIGURATION
 # ------------------------------------------------------------------------------
