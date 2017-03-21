1193f46c6e98af0847626df35c216fc42d102f1c
Ben Creighton <bncreighton@gmail.com>
2017-03-20 22:19:43 -0700

    Draft for RideRequest model

--- up_ride_finder/rides/models.py
+++ up_ride_finder/rides/models.py
@@ -13,19 +13,27 @@ from .validators import valid_zip, future_date
 
 
 class Ride(models.Model):
-    destination = models.CharField(_('ZIP code of final destination'), max_length=5, validators=[valid_zip])
-    origin = models.CharField(_('ZIP code of origin'), max_length=5, validators=[valid_zip])
-    available_seats = models.IntegerField(_('Maximum number of passengers'), validators=[MinValueValidator(1)])
-    cost = models.CharField(_('Approx. cost the driver expects riders to pay.'), max_length=255)
-    when = models.DateTimeField(_('Approximate departure date and time'), validators=[future_date])
+    destination = models.CharField(
+        _('ZIP code of final destination'), max_length=5, validators=[valid_zip])
+    origin = models.CharField(_('ZIP code of origin'),
+                              max_length=5, validators=[valid_zip])
+    available_seats = models.IntegerField(
+        _('Maximum number of passengers'), validators=[MinValueValidator(1)])
+    cost = models.CharField(
+        _('Approx. cost the driver expects riders to pay.'), max_length=255)
+    when = models.DateTimeField(
+        _('Approximate departure date and time'), validators=[future_date])
     trip_summary = models.CharField(_('Summary of the trip'), max_length=255)
-    driver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='driver')
+    driver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='driver')
     riders = models.ManyToManyField(User, related_name='riders', blank=True)
     created_date = models.DateTimeField(_('Date created'), auto_now_add=True)
     modified_date = models.DateTimeField(_('Date modified'), auto_now=True)
 
-    dest_name = models.CharField(_('Friendly name of final destination'), max_length=255, blank=True)
-    origin_name = models.CharField(_('Friendly name of origin'), max_length=255, blank=True)
+    dest_name = models.CharField(
+        _('Friendly name of final destination'), max_length=255, blank=True)
+    origin_name = models.CharField(
+        _('Friendly name of origin'), max_length=255, blank=True)
     # trip_id = models.PositiveIntegerField(_('Trip ID'))
     # Django has an automatic id field
 
@@ -40,7 +48,8 @@ class Ride(models.Model):
         Set the origin and destination to friendlier names if possible.
         """
         if not self.dest_name or not self.origin_name:
-            gmaps = googlemaps.Client(key=getattr(settings, 'GOOGLE_MAPS_GEOCODING_KEY', None), timeout=5)
+            gmaps = googlemaps.Client(key=getattr(
+                settings, 'GOOGLE_MAPS_GEOCODING_KEY', None), timeout=5)
             if not self.dest_name:
                 gmaps_results = gmaps.geocode(self.destination)
                 dest_name = ''
@@ -61,3 +70,26 @@ class Ride(models.Model):
                     self.origin_name = self.origin
 
         super(Ride, self).save(*args, **kwargs)  # Call the real save() method
+
+
+class RideRequest(models.Model):
+    ACCEPTED = 'AC'
+    DECLINED = 'DE'
+    UNDECIDED = 'UN'
+    STATUS_CHOICES = (
+        (ACCEPTED, 'Accepted'),
+        (DECLINED, 'Declined'),
+        (UNDECIDED, 'Undecided'),
+    )
+    ride = models.ForeignKey(
+        Ride, on_delete=models.CASCADE, related_name='ride')
+    requester = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='requester')
+    status = models.CharField(
+        max_length=2, choices=STATUS_CHOICES, default=UNDECIDED)
+
+    def is_decided(self):
+        return self.status != self.UNDECIDED
+
+    def __str__(self):
+        return "User {}'s {} request for ride {}".format(self.requester_id, self.get_status_display(), self.ride_id)
