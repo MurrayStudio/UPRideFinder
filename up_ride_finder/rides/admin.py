ac224ea106442f8dd692aeef6249cea3df144f84
Ben Creighton <bncreighton@gmail.com>
2017-03-20 22:33:50 -0700

    Basic RideRequest Admin form

--- up_ride_finder/rides/admin.py
+++ up_ride_finder/rides/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Ride
-from .forms import RideAdminForm
+from .models import Ride, RideRequest
+from .forms import RideAdminForm, RideRequestAdminForm
 # Register your models here.
 
 
 class RideAdmin(admin.ModelAdmin):
     form = RideAdminForm
 
+
+class RideRequestAdmin(admin.ModelAdmin):
+    form = RideRequestAdminForm
+
 admin.site.register(Ride, RideAdmin)
+admin.site.register(RideRequest, RideRequestAdmin)
--- up_ride_finder/rides/forms.py
+++ up_ride_finder/rides/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
-from .models import Ride
+from .models import Ride, RideRequest
 
 
 class RideAdminForm(forms.ModelForm):
@@ -11,11 +11,25 @@ class RideAdminForm(forms.ModelForm):
         fields = '__all__'
 
     def clean(self):
-        """ Prevent the driver from being a rider """
+        """Prevent the driver from being a rider """
         driver = self.cleaned_data.get('driver')
         riders = self.cleaned_data.get('riders')
         if driver in riders:
             raise ValidationError(_('Trip driver cannot be a rider.'))
+        return self.cleaned_data
+
+
+class RideRequestAdminForm(forms.ModelForm):
+    class Meta:
+        model = RideRequest
+        fields = '__all__'
+
+    def clean(self):
+        """Prevent the driver from being a requester """
+        ride = self.cleaned_data.get('ride')
+        requester = self.cleaned_data.get('requester')
+        if requester == ride.driver:
+            raise ValidationError(_('Trip driver cannot be a requester.'))
         return self.cleaned_data
 
 
