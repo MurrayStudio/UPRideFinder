b35866b3ac7fb5b841890a4c0f8a039041d55e2e
Murray Studio <s.murray.studio@gmail.com>
2017-03-23 21:57:22 -0700

    Work on Json for Google Maps.

--- up_ride_finder/static/js/places.js
+++ up_ride_finder/static/js/places.js
@@ -1,24 +1,28 @@
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
-          center: {lat: -34.397, lng: 150.644}
-        });
-        var geocoder = new google.maps.Geocoder();
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: { lat: -34.397, lng: 150.644 }
+  });
+  var geocoder = new google.maps.Geocoder();
 
-          geocodeAddress(geocoder, map);
-      }
+  $.getJSON(window.location.href + "json", function (data) {
+    console.log(data);
+  });
 
-      function geocodeAddress(geocoder, resultsMap) {
-        var address = "Portland, Or";
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }
+  geocodeAddress(geocoder, map);
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = "Portland, Or";
+  geocoder.geocode({ 'address': address }, function (results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
